612922a97c803e00af71afc33de47406a3482d32
ASMFAISAL1994 <asmfaisal1994@gmail.com>
2023-11-14 14:53:02 +0600

    first commit

---
+++ src/main/java/Core/CoreConstrainHelper.java
@@ -0,0 +1,9 @@
+package Core;
+
+public class CoreConstrainHelper {
+    public static String base_url = "https://reqres.in/api/";
+
+//    public static String base_url = "https://bookstore.demoqa.com/";
+
+
+}
